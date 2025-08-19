b7f591d47b082870f8cd2bb5e81626475f490231
Suikyoo <judeanthony02sayson@gmail.com>
2025-08-19 16:11:52 +0800

    done going to school

---
+++ web/social/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<main class="box-border p-5 w-screen">
+    <div class="bg-slate-200 w-full h-1/3 flex-col justify-center items-center p-20 rounded-xl">
+        <h1 class="text-black text-center">So...</h1>
+    </div>
+</main>
