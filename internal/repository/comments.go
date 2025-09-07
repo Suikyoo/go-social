bce96720772eb2dd6436a8e2db1d0b364bd9dc8c
Suikyo <judeanthony02sayson@gmail.com>
2025-09-07 22:49:44 +0800

    trying to make the comment system

--- internal/repository/comments.go
+++ internal/repository/comments.go
@@ -1,1 +1,47 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+type Comment struct {
+  ID int64 `json:"id"`
+  UserID int64 `json:"user_id"`
+  PostID int64 `json:"post_id"`
+  Username string `json:"username"`
+  Content string `json:"content"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+  Likes int64 `json:"likes"`
+}
+
+type CommentRepository interface {
+  RegularRepository[Comment]
+}
+
+type SqlCommentRepository struct {
+  db *sql.DB
+}
+
+  /*
+func (r *SqlCommentRepository) GetFeed(ctx context.Context, amt int8) ([]Comment, error) {
+  query := `
+  SELECT users.username, users.id, posts.id, comments.id, comments.content, comments.created_at, comments.updated_at
+  FROM comments
+  INNER JOIN users ON comments.user_id = users.id
+  INNER JOIN posts ON comments.post_id = posts.id
+  LIMIT $1
+  `
+  ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+  defer cancel()
+
+  rows, err := r.db.QueryContext(
+    ctx,
+    query,
+    amt,
+  )
+
+}
+  */
--- web/social/src/lib/pages/PostCreation.svelte
+++ web/social/src/lib/pages/PostCreation.svelte
@@ -29,7 +29,7 @@
   <div class="flex flex-col items-center w-full max-w-[640px] bg-inherit">
     <h1 class="self-start">Create Post</h1>
     <Input name="Title" type="text" className="w-full h-[4em]" bind:value={post.title} />
-    <TextArea rows=20 name="Content" type="text" className="w-full h-[10em] bind:value={post.content} "/>
+    <TextArea rows=20 name="Content" type="text" className="w-full h-[10em]" bind:value={post.content}/>
     <p bind:this={feedback}></p>
     <Button className="bg-blue-700 focus-visible:outline-blue-700 hover:outline-blue-700 self-end">Submit</Button>
   </div>
