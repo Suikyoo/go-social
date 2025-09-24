f088c794204a8416dc607bf550ba3d481c11e9c2
Suikyoo <judeanthony02sayson@gmail.com>
2025-09-25 07:12:10 +0800

    working on postview

--- web/social/src/lib/pages/PostView.svelte
+++ web/social/src/lib/pages/PostView.svelte
@@ -4,17 +4,27 @@
   import { type Comment, fetchCommentFeed, createComment} from "../types/comment";
   import Input from "../components/Input.svelte";
   import Table from "@components/Table.svelte"
+  import {onMount} from "svelte";
 
   let { route } = $props();
   let id: string = route.result.path.params.id;
+
+  let commentFeed: Comment[] = $state([]);
+
   let comment: Comment =  $state({post_id: id, content: ""});
 
   async function create() {
     let err = await createComment(comment);
     if (!err) {
+      commentFeed.push({...comment})
       comment.content = "";
     }
   }
+
+  onMount(() => {
+    fetchCommentFeed(id).then((feed) => commentFeed = feed)
+  })
+
 </script>
 
 <main class="size-full flex flex-col justify-start box-border p-5 border-zinc-800">
@@ -35,28 +45,22 @@
       <div class="h-50">{post.content}</div>
     </Table>
 
-    {#await fetchCommentFeed(id)}
-      <p>fetching comments...</p>
-
-    {:then feed} 
-      <Table className="grid-rows-1 text-left border-[0.5px] border-zinc-800">
-        <div>Comments</div>
-        {#each feed as comment}
-          <div>
-            {comment.username}: {comment.content}
-          </div>
-        {/each}
-        <div class="flex flex-row justify-between items-center w-full">
-          <Input type="text" bind:value={comment.content} className="h-[2em] w-full" nativeClass="border-zinc-600 border-1"/>
-          <button onclick={create} class="hover:outline-1 m-2 p-1 rounded-sm">
-            <Send />
-          </button>
-
+    <Table className="grid-rows-1 text-left border-[0.5px] border-zinc-800"> 
+      <div>Comments</div>
+      {#each commentFeed as comment}
+        <div class="flex flex-col w-full border-red-200">
+          {comment.username}: {comment.content}
         </div>
+      {/each}
+      <div class="flex flex-row justify-between items-center w-full">
+        <Input type="text" bind:value={comment.content} className="h-[2em] w-full" nativeClass="border-zinc-600 border-1"/>
+        <button onclick={create} class="hover:outline-1 m-2 p-1 rounded-sm">
+          <Send />
+        </button>
 
-      </Table>
+      </div>
 
-    {/await}
+    </Table>
 
   {:catch err}
     <p>{err}</p>
